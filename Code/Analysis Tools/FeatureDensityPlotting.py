5df07ad0f9fe9740d75b47da28e60dbb3d9bf8d6
Timon Bauer <tibauer@uni-potsdam.de>
2021-07-21 16:08:38 +0200

    removing not used package

--- Code/Analysis Tools/FeatureDensityPlotting.py
+++ Code/Analysis Tools/FeatureDensityPlotting.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import seaborn as sns
 import sys
 
 class FeatureDensityPlotting (object):
--- Code/Predicting/CentralPositionFinder.py
+++ Code/Predicting/CentralPositionFinder.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
 import pandas as pd
-import seaborn as sns
 import sys
 sys.path.insert(0, "./Code/Feature and Label Creation/")
 
