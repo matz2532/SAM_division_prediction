84c36b2531379b7394b6948fb7258a4e29bee616
Timon Bauer <tibauer@uni-potsdam.de>
2021-07-21 13:23:12 +0200

    checking number of dividing of all cells from layer one of Jackson et al., 2019

---
+++ Code/analysisOfJacksonsUnderlyingData.py
@@ -0,0 +1,103 @@
+import glob
+import os
+import numpy as np
+import pandas as pd
+
+class GetDividingAndNonDividingLabels (object):
+
+    def __init__(self, topologyFilenames, parentFilename, givePattern=True):
+        self.sep = ","
+        self.givePattern = givePattern
+        self.setupTables(topologyFilenames, parentFilename)
+
+    def setupTables(self, topologyFilenames, parentFilename):
+        if self.givePattern:
+            self.topologyFilenames = glob.glob(topologyFilenames)
+        else:
+            self.topologyFilenames = topologyFilenames
+        self.topologyTables = self.loadTables(self.topologyFilenames)
+        if self.givePattern:
+            self.parentFilename = glob.glob(parentFilename)
+        else:
+            self.parentFilename = parentFilename
+        self.parentTables = self.loadTables(self.parentFilename)
+
+    def loadTables(self, filenames):
+        tables = []
+        for filename in filenames:
+            table = pd.read_csv(filename, sep=self.sep)
+            tables.append(table)
+        return tables
+
+    def ExtractDividingCells(self, selectedLayer=None):
+        self.dividingCellsPerLayer = {}
+        self.nrOfCellsPerLayer = {}
+        for i, parentalDf in enumerate(self.parentTables):
+            currentParentalDfFilename = self.topologyFilenames[i]
+            layerDf = self.topologyTables[i]
+            currentTissuesDividingCellsAndLayer = self.extractDividingCells(parentalDf, layerDf)
+            self.dividingCellsPerLayer = self.combineDicts(self.dividingCellsPerLayer, currentTissuesDividingCellsAndLayer)
+            if "T0" in currentParentalDfFilename:
+                nNumbersOfCellsPerTissue = self.extractNrOfCellsPerTissueLayer(layerDf)
+                self.nrOfCellsPerLayer = self.combineDicts(self.nrOfCellsPerLayer, nNumbersOfCellsPerTissue)
+        if selectedLayer:
+            self.printDivdidingCellsPerLayer(selectedLayer, self.dividingCellsPerLayer, self.nrOfCellsPerLayer)
+        else:
+            for selectedLayer in self.dividingCellsPerLayer.keys():
+                self.printDivdidingCellsPerLayer(selectedLayer, self.dividingCellsPerLayer, self.nrOfCellsPerLayer)
+
+    def extractDividingCells(self, parentalDf, layerDf):
+        parentalCells = parentalDf.loc[:, " Parent Label"]
+        uniqueParents, counts = np.unique(parentalCells, return_counts=True)
+        dividingCells = uniqueParents[counts > 1]
+        isDivCellInLayerDf = np.isin(layerDf.loc[:, "Label"], dividingCells)
+        layerOfDividingCells = layerDf.loc[isDivCellInLayerDf, "Layer"]
+        currentTissuesDividingCellsAndLayer = {}
+        for layer in np.unique(layerOfDividingCells):
+            dividingCellsOfCurrentLayer = dividingCells[layerOfDividingCells==layer]
+            currentTissuesDividingCellsAndLayer[layer] = list(dividingCellsOfCurrentLayer)
+        return currentTissuesDividingCellsAndLayer
+
+    def combineDicts(self, dict1, dict2):
+        for key, values in dict2.items():
+            assert isinstance(values, list), "The values {} of key {} in dict2 {} need to be list, {} != list".format(dict2, key, type(values))
+            if key in dict1:
+                assert isinstance(dict1[key], list), "The values {} of key {} in dict1 {} need to be list, {} != list".format(dict1, key, type(dict1[key]))
+                dict1[key].extend(values)
+            else:
+                dict1[key] = values
+        return dict1
+
+    def extractNrOfCellsPerTissueLayer(self, layerDf):
+        nrOfCellsPerTissueLayer = {}
+        for layer in np.unique(layerDf.loc[:, "Layer"]):
+            nrOfCellsPerTissueLayer[layer] = list(layerDf.loc[layerDf.loc[:, "Layer"]==layer, "Label"])
+        return nrOfCellsPerTissueLayer
+
+    def printDivdidingCellsPerLayer(self, selectedLayer, dividingCellsPerLayer, nrOfCellsPerLayer):
+        nrOfDividingCells = len(dividingCellsPerLayer[selectedLayer])
+        totalCellNumber =  len(nrOfCellsPerLayer[selectedLayer])
+        percentage = np.round(100*nrOfDividingCells/totalCellNumber, 1)
+        print("In layer: {}, {} / {} divided ({}%)".format(selectedLayer, nrOfDividingCells, totalCellNumber, percentage))
+
+def main():
+    # print number of dividing cells of all cells in Jackson et al., 2019
+    # I found out that the label dividing is propagated
+    # meaning cells dividing in the first time point, e.g. label 362 (T0), splits into 1276 and 292 (T1),
+    # but in the file "02_T1_topology.csv" the cells 1276 and 292 are marked with 1 in the Division column
+    # therefore I check the parents labels of e.g. "02_T0-T1_parents.csv", "02_T1-T2_parents.csv" for multiple entries
+    # download 'WT Arabidopsis.zip' data from https://osf.io/h587k/ (see STAR★Methods of Jackson et al., 2019)
+    # and copy this script into the base folder (WT Arabidopsis) containing the 'labels.csv' and 'README.txt' file
+    # and execute the script
+    import sys
+    filenameToSave = "labels.csv"
+    for i in range(1,5):
+        print("Replicate {}".format(i))
+        replicateFolder = "WT Arabidopsis/Rep {}/*/".format(i)
+        topologyPatterns = replicateFolder + "*topology.csv"
+        parentPatterns = replicateFolder + "*parents.csv"
+        myGetDividingAndNonDividingLabels = GetDividingAndNonDividingLabels(topologyPatterns, parentPatterns)
+        myGetDividingAndNonDividingLabels.ExtractDividingCells()
+
+if __name__ == '__main__':
+    main()
