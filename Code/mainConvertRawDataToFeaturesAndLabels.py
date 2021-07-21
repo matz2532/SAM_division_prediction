2fa49da076063f300f1af8a6be33308e533b6681
Timon Bauer <tibauer@uni-potsdam.de>
2021-07-21 16:31:34 +0200

    combining WT and ktn feature and label creation into one file

--- Code/mainConvertRawDataToFeaturesAndLabels.py
+++ Code/mainConvertRawDataToFeaturesAndLabels.py
@@ -5,9 +5,27 @@ from CreateFeatureSets import CreateFeatureSets
 
 def mainConvertRawDataToFeaturesAndLabels():
     createDivEventData = False
-    centralCellsDict = {"ktnP1": [ [], [3839, 3959] ],
-                        "ktnP2": [ [23], [424, 426, 50] ],
-                        "ktnP3": [ [29, 199,527], [424, 28, 431] ] }
+    WtCentralCellsDict = {"P1":[[618, 467, 570], [5048, 5305], [5849, 5601], [6178, 6155, 6164], [6288, 6240]],
+                        "P2":[[392], [553, 779, 527], [525], [1135], [1664, 1657]],
+                        "P5":[[38], [585, 968, 982], [927, 1017], [1136], [1618, 1575, 1445]],
+                        "P6":[[861], [1651, 1621], [1763, 1844], [2109, 2176], [2381]],
+                        "P8":[[3241, 2869, 3044], [3421, 3657], [2805, 2814, 2876], [3013], [358, 189]]}
+    wtDivDataArgs = {"dataFolder":"Data/WT/",
+                   "folderToSave":"Data/WT/divEventData/",
+                   "plantNames":["P1", "P2", "P5", "P6", "P8"],
+                   "estimateFeatures":True,
+                   "estimateLabels":True,
+                   "useManualCentres":True}
+    wtTopoDataArgs = {"dataFolder":"Data/WT/",
+                   "folderToSave":"Data/WT/topoPredData/diff/",
+                   "plantNames":["P1", "P2", "P5", "P6", "P8"],
+                   "estimateFeatures":True,
+                   "estimateLabels":True,
+                   "useManualCentres":True,
+                   "useTopoCreator":True}
+    ktnCentralCellsDict = {"ktnP1": [ [], [3839, 3959] ],
+                           "ktnP2": [ [23], [424, 426, 50] ],
+                           "ktnP3": [ [29, 199,527], [424, 28, 431] ] }
     ktnDivDataArgs = {"dataFolder": "Data/ktn/",
                    "folderToSave": "Data/ktn/divEventData/",
                    "plantNames": ["ktnP1", "ktnP2", "ktnP3"],
@@ -25,12 +43,21 @@ def mainConvertRawDataToFeaturesAndLabels():
                    "timePointsPerPlant": 3,
                    "useTopoCreator": True,
                    "takeCorrelationFromDifferentFolder":"Data/WT/topoPredData/diff/manualCentres/"}
-    if createDivEventData:
+    for usingWT, createDivData in [[True, False], [True, True], [False, False], [False, True]]:
+        if usingWT:
+            centralCellsDict = WtCentralCellsDict
+            if createDivData:
+                dataArgs = wtDivDataArgs
+            else:
+                dataArgs = wtTopoDataArgs
+        else:
+            centralCellsDict = ktnCentralCellsDict
+            if createDivData:
+                dataArgs = ktnDivDataArgs
+            else:
+                dataArgs = tktnTopoDataArgs
         CreateFeatureSets(skipEmptyCentrals=True, centralCellsDict=centralCellsDict,
-                          **ktnDivDataArgs)
-    else:
-        CreateFeatureSets(skipEmptyCentrals=True, centralCellsDict=centralCellsDict,
-                          **tktnTopoDataArgs)
+                          **dataArgs)
 
 if __name__== "__main__":
     mainConvertRawDataToFeaturesAndLabels()
