10862b48d74c420d07cc8deb2c05ee73202cbb41
Timon Bauer <tibauer@uni-potsdam.de>
2022-03-25 11:48:02 +0100

    added missing correction for p-values of feature correlation analysis

--- Code/mainAnalyseOccurrenceAndPerformance.py
+++ Code/mainAnalyseOccurrenceAndPerformance.py
@@ -77,5 +77,5 @@ def plotAndPrepareSuppFigures(resultsFolder="Results/SuppFigures/", figuresToDo=
 
 
 if __name__== "__main__":
-    plotAndPrepareMainFigures()
-    # plotAndPrepareSuppFigures()
+    # plotAndPrepareMainFigures()
+    plotAndPrepareSuppFigures()
