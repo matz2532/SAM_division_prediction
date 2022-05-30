2037a147753782b42e3ca95a0890a75428093cc8
Timon Bauer <tibauer@uni-potsdam.de>
2022-05-30 20:52:22 +0200

    create and run mainRunTissuePropagation all except randomised propagation; creating fig 4 C, E, and Sup Fig 8 parts creating results for fig 4 D (predicted vs observed)

---
+++ Code/mainRunTissuePropagation.py
@@ -0,0 +1,22 @@
+import sys
+
+sys.path.insert(0, "./Code/Propagation/")
+
+from DivAndTopoPredictor import propagateAndCorrelateTissues
+from LocalTopologyPredictionComparer import compareLocalTopologyPrediction
+from mainAnalyseOccurrenceAndPerformance import plotAndPrepareMainFigures
+from RandomisedTissuePrediction import mainRandomiseWithMultiplePlants
+
+def main():
+    print("Do calculations for figure 4C")
+    compareLocalTopologyPrediction()
+    plotAndPrepareMainFigures(figuresToDo="Fig. 4 C")
+    print("Do calculation for figure D (predicted vs observed), E, and Supp Fig 8")
+    propagateAndCorrelateTissues()
+    plotAndPrepareMainFigures(figuresToDo="Fig. 4 D")
+    plotAndPrepareMainFigures(figuresToDo="Fig. 4 E") # and Sup. Fig. 8
+    print("Do calculation for figure D (random vs observed)")
+    mainRandomiseWithMultiplePlants()
+
+if __name__== "__main__":
+    main()
